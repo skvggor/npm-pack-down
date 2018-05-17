39270d78255ba881e0a289f16ec08cb367c83a65
Marcos Garcia de Lima <marckfree@gmail.com>
2018-05-17 17:04:41 -0300

    Add reqwest, serde (request to API)

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,26 @@
-#[macro_use] extern crate futures;
-#[macro_use] extern crate hyper;
-#[macro_use]extern crate tokio_core;
-#[macro_use] extern crate nickel;
+#[macro_use] extern crate serde_derive;
+extern crate serde;
+// extern crate serde_json;
 
-use std::env;
-use std::collections::HashMap;
+extern crate reqwest;
 
-use futures::{Future};
-use hyper::{Client, Uri};
-use tokio_core::reactor::Core;
+#[derive(Debug, Deserialize)]
+struct Request {
+  downloads: Vec<Downloads>
+}
 
-use nickel::{Nickel, StaticFilesHandler, HttpRouter};
-// use nickel::extensions::{Referer, Redirect};
-
-fn get_server_port() -> u16 {
-    env::var("PORT").unwrap_or("6767".to_string()).parse().unwrap()
+#[derive(Debug, Deserialize)]
+struct Downloads {
+    downloads: i64
 }
 
 fn main() {
-    let mut server = Nickel::new();
-    let mut router = Nickel::router();
+    let start_date = "2018-04-08";
+    let end_date = "2018-05-16";
+    let package_name = "begynner";
+    let url = format!("https://api.npmjs.org/downloads/range/{}:{}/{}", start_date, end_date, package_name);
 
-    let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let mut request = reqwest::get(&url).unwrap();
 
-    let url : Uri = "http://httpbin.org/ip".parse().unwrap();
-
-    let request = client.get(url)
-        .map(|res| {
-            return res.body();
-    });
-
-    core.run(request).unwrap();
-
-    router.get("/", middleware! { |_, response|
-        let mut data = HashMap::new();
-        data.insert("response", "Teste");
-        return response.render("views/index.tpl", &data);
-    });
-
-    // res.body().for_each(|chunk| {
-    //     assert_eq!(res.status(), hyper::Ok);
-    // });
-
-    server.utilize(StaticFilesHandler::new("assets"));
-    server.utilize(StaticFilesHandler::new("views"));
-    server.utilize(router);
-
-    server.listen(("0.0.0.0", get_server_port()));
+    println!("{:?}", request.json::<Request>());
 }
